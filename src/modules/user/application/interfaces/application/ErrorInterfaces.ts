b00f6796b8676bdbe53244875d1eafdaf2643cd6
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-11-24 11:11:21 -0300

    Create error interfaces

---
+++ src/modules/user/application/interfaces/application/ErrorInterfaces.ts
@@ -0,0 +1,11 @@
+export interface IControllerErrors {
+  CREATE_ERROR: string;
+  UPDATE_ERROR: string;
+  DELETE_ERROR: string;
+  FIND_ERROR: string;
+  FIND_ALL_ERROR: string;
+  USER_ALREADY_EXISTS: string;
+  USER_NOT_FOUND: string;
+  MISSING_FIELDS: string;
+  INVALID_BODY_TYPE: string;
+}
